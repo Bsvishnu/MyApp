d188757b0fb04dca7766e5b961cc76bc0529699d
Vishnu Soni <>
2022-01-24 10:56:11 +0530

    updated jdk version to 11

--- myapplication/src/main/java/com/bigstep/mylibrary/TestActivity.kt
+++ myapplication/src/main/java/com/bigstep/mylibrary/TestActivity.kt
@@ -2,6 +2,7 @@ package com.bigstep.mylibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bigstep.playLib.R
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
